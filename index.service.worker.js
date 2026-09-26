2c9e5e7c6d5a767c992c2fbf30463d8bfe20a286
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2026-09-26 13:52:05 +0300

    updates

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1790416385|2338647';
+const CACHE_VERSION = '1790419821|2345329';
 /** @type {string} */
 const CACHE_PREFIX = 'Arcade Soccer-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
