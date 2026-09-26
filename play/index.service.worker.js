3dd747adcad6fe00a4ba1504ef7f5b11f853dbec
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2026-09-26 15:20:37 +0300

    Publish Mega Soccer site 2026-09-26 15:20

--- play/index.service.worker.js
+++ play/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1790423975|2817427';
+const CACHE_VERSION = '1790425139|3794652';
 /** @type {string} */
 const CACHE_PREFIX = 'Arcade Soccer-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
